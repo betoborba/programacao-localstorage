936246918890dd9dc61eb79896895fb9c1e4baaf
Roberto Borba <r.robertoborba@gmail.com>
2026-09-17 17:50:12 -0300

    atualização

--- js/ex11_3lista.js
+++ js/ex11_3lista.js
@@ -24,5 +24,34 @@ const montarTabela = () => {
     }
 };
 
+// ocorre quando checkbox é alterado. Exibe a lista se marcado, senão, recarrega. 
 
+ckMostrar.addEventListener("change", () => {
+    //se a classe do elemento alvo clicado contém exclui
+    if (e.target.classList.contains("exclui")) {
+        // acessa o "pai do pai" do elemento alvo, e obtém o texto do 1º filho
+        const palavra = e.target.parentElement.parentElement.children[0].innerText;
+
+        if (confirm(`Confirma Exclusão da Palavra: "${palavra}"?`)) {
+            // remove a linha da tabela, correspondente ao símbolo de excluir clicado
+            e.target.parentElement.parentElement.remove();
+
+            localStorage.removeItem("jogoPalavra"); // Exclui dados de localStorage
+            localStorage.removeItem("jogoDica");
+
+            const palavras = [];
+            const dicas = [];
+
+            // obtém dos dados da tabela, acrescentando-os aos vetores
+            for (let i = 1; i < tbPalavras.rows.length; i++) {
+                palavras.push(tbPalavras.rows[i].cells[0].innerText);
+                dicas.push(tbPalavras.rows[i].cells[i].innerText);
+            }
+
+            // salva o conteúdo dos vetores em localStorage (sem a linha removida)
+            localStorage.setItem("jogoPalavra", palavras.join(";"));
+            localStorage.setItem("jogoDica", dicas.join(";"));
+        }
+    }
+});
 
